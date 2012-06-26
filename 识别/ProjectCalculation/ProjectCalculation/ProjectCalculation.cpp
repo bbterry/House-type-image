aac037e4b0f8a6de28e242d49d8d1185e5d9a3d5
chenwz <862910030@qq.com>
2012-06-26 11:11:38 +0800

    cal

---
+++ 识别/ProjectCalculation/ProjectCalculation/ProjectCalculation.cpp
@@ -0,0 +1,26 @@
+#include "ProjectCalculation.h"
+
+ProjectCalculation::ProjectCalculation()
+{
+
+}
+
+ProjectCalculation::~ProjectCalculation()
+{
+
+}
+
+bool ProjectCalculation::readXML(const char * filename)
+{
+
+}
+
+point * ProjectCalculation::findContour(vector<segment> sgm)
+{
+
+}
+
+void ProjectCalculation::calculate()
+{
+
+}
---
+++ 识别/ProjectCalculation/ProjectCalculation/main.cpp
@@ -0,0 +1,9 @@
+#include "ProjectCalculation.h"
+#include <iostream>
+
+void main(int agc, char ** agv)
+{
+	char * filename = (agc == 2) ? agv[1] : "elements.xml";
+	ProjectCalculation calc;
+	if(!calc.readXML(filename)) return;
+}
